6edac479f3fab785be817d558dae07c831734c44
Dragos Morar <pronoob1029@gmail.com>
2026-09-13 10:36:13 +0300

    brought lib

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/TimeKeep.kt
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.library
+
+import com.commonlibs.units.Time
+import com.commonlibs.units.ms
+
+class TimeKeep {
+    private var isInitialized = false
+
+    var previousTime = timeNow() - 1.ms
+    var currentTime = timeNow()
+    inline val deltaTime get() = currentTime - previousTime
+
+    fun resetDeltaTime() {
+        if (isInitialized.not()) {
+            isInitialized = true
+            currentTime = timeNow()
+            previousTime = currentTime - 1.ms
+            return
+        }
+
+        previousTime = currentTime
+        currentTime = timeNow()
+    }
+
+    private fun timeNow() = Time.now()
+}
